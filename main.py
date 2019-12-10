6565999ec5a90bd270c7452886d7cef9c0b342fb
andrazpovse <andraz.povse1@gmail.com>
2019-12-10 16:41:07 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,64 @@
+import vlc
+import time
+
+def play(p):
+    '''
+        Plays the entire music track
+        @params
+            p - media player instance with set media file
+    '''
+    p.play() 
+    vlc.libvlc_audio_set_volume(p, 100)  # volume 0..100
+    time.sleep(1)
+    duration = p.get_length() / 1000
+
+    for i in range(int(duration)):
+        time.sleep(1)
+        # Change volume based on light. Can also break the loop if a certain threshold
+        # is reached.
+        if i > 5:
+            vlc.libvlc_audio_set_volume(p, 50)
+        
+        print("Playing for ", i, "seconds.")
+
+    return
+
+
+def set_music(instance, file):
+    '''
+        Sets the music to play.
+        @params
+            instance - VLC instance
+            file - path to file(mp3, others?try)
+        @returns
+            media file for instance to play
+    '''
+    m = instance.media_new(file)
+
+    return m
+
+def detect_movement():
+    # TODO
+    pass
+
+def light_status():
+    # TODO
+    # Returns some value correlated to brightness
+    pass
+
+if __name__ == "__main__":
+    # Init VLC
+    instance = vlc.Instance('--aout=alsa')
+    p = instance.media_player_new()
+
+    # Set the music file
+    p.set_media(set_music(instance, 'test.mp3')) 
+
+
+    # infinite loop that plays music when movement is detected
+    while True:
+        # TODO Trigger play upon movement detected by sensor
+        play(p)
+        # Set the media to play next...can be based on current light
+        p.set_media(set_music(instance, 'test.mp3')) 
+
