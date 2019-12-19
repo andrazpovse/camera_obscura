1988b18dbcd4e2868be39ecf8979673c772a2165
andrazpovse <andraz.povse1@gmail.com>
2019-12-19 08:21:01 +0100

    Changed location of run_at_boot target python script.

--- run_at_boot.sh
+++ run_at_boot.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=$PYTHONPATH:/home/pi/.local/lib/python3.7/site-packages
 # Script that will be starting after bootup (cronjob).
 # Starts the python code.
 cd /
-cd home/pi/seminarska2
+cd home/pi/camera_obscura
 sudo python3 main.py
 cd /
