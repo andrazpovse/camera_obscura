6522197baa6471d9ef6da7109b4ace67ca32836c
Denis Vitez <denis.vitez@gmail.com>
2020-01-11 16:05:23 +0100

    Sounds are now concatenated/generated.

--- web-interface/src/components/CameraObscura.vue
+++ web-interface/src/components/CameraObscura.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
+import Crunker from 'crunker';
 export default {
     name: 'CameraObscura',
     props: {
@@ -15,56 +16,40 @@ export default {
     data: function() {
        return {
           lightIntensity: 0,
+          audio: new Crunker({sampleRate: 48000}),
+          buffers: null
         }
     },
     mounted: function () {
+        this.audio.fetchAudio('sounds/single_click.wav', 'sounds/single_crackle_slower.wav', 'sounds/single_crackle_faster.wav', 'sounds/double_crackle.wav')
+            .then(x => {
+                this.buffers = x;
+                // eslint-disable-next-line no-console
+                console.log("Buffers:", this.buffers);
+            });
         this.$nextTick(function () {
             window.setInterval(() => {
                 // eslint-disable-next-line no-console
                 console.log("Playing...");
-                var converted = this.convertToLogarithm(5000);
-                // eslint-disable-next-line no-console
-                console.log("Converted value:", converted);
-                var limits = this.generateLimits(converted);
-                // eslint-disable-next-line no-console
-                console.log("Limits:", limits);
-                this.playFile("sounds/single_click.wav");
-                /*
                 axios.get(`http://vitez.si:8086/query?pretty=true&db=ioi&q=SELECT%20Last(intensity)%20from%20lightIntensity`)
                 .then(response => {
                     // JSON responses are automatically parsed.
-                    // eslint-disable-next-line no-console
-                    console.log(response);
                     var intensity = response.data.results[0].series[0].values[0][1];
                     // eslint-disable-next-line no-console
                     console.log(intensity);
                     this.lightIntensity = intensity;
-                    this.playSound(intensity);
+                    var converted = this.convertToLogarithm(intensity);
+                    var limits = this.generateLimits(converted);
+                    this.createAndPlaySound(limits);
                 })
                 .catch(e => {
                     // eslint-disable-next-line no-console
                     console.log(e);
                 })
-                */
             }, 2000);
         })
     },
     methods: {
-        playSound(value) {
-            if(value < 100) {
-                this.playFile("sounds/low.mp3");
-            } else if(value < 1000) {
-                this.playFile("sounds/med.mp3");
-            } else {
-                this.playFile("sounds/high.mp3");
-            }
-        },
-        playFile (file) {
-            if(file) {
-                var audio = new Audio(file);
-                audio.play();
-            }
-        },
         convertToLogarithm(value) {
             return Math.log10(value) * 100;
         },
@@ -94,8 +79,39 @@ export default {
             }
             return {crackle: crackleLimit, upper: upperSilenceLimit, lower: lowerSilenceLimit};
         },
-        createSound(limits) {
-            return limits;
+        randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        silentBuffer(duration) {
+            var createBuffer = require('audio-buffer-from');
+            var buff = createBuffer(48*duration, {channels: 2, sampleRate: 48000});
+            return buff;
+        },
+        createAndPlaySound(limits) {
+            /*
+            Buffers
+            0 -> single_click
+            1 -> single_crackle_slow
+            2 -> single_crackle_fast
+            3 -> double_crackle
+            */
+            var sound = this.silentBuffer(10);
+            var sampleLength = 2000;
+            var totalLength = 0;
+            while(totalLength < sampleLength) {
+                var silencePeriod = this.randomNumber(limits.lower, limits.upper);
+                totalLength += silencePeriod;
+                sound = this.audio.concatAudio([sound, this.silentBuffer(silencePeriod)]);
+                if(this.randomNumber(1, 100) < limits.crackle) {
+                    var crackleIndex = this.randomNumber(1, 3);
+                    sound = this.audio.concatAudio([sound, this.buffers[crackleIndex]]);
+                    totalLength += this.buffers[crackleIndex].duration * 1000;
+                } else {
+                    sound = this.audio.concatAudio([sound, this.buffers[0]]);
+                    totalLength += this.buffers[0].duration * 1000;
+                }
+            }
+            this.audio.play(sound);
         }
     }
 }
